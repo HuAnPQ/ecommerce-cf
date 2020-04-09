daa2f682ac09ed982d439151b9eca25712dc2c50
Hugo Ponce <hugoponcequiroz@gmail.com>
2020-04-08 21:51:55 -0500

    Autenticacion y productos - cf

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app');
+
+@section('content')
+<div class="container">
+    <div class="row">
+        @foreach ($products as $product)
+        <div class="col-xs-12 col-sm-6 col-md-4">
+            <div class="card padding">
+                <header>
+                    <h2 class="card-title">
+                        <a href="/productos/{{$product->id}}">{{$product->title}}</a>
+                    </h2>
+                    <h4 class="card-subtitle">{{$product->price}}</h4>
+                </header>
+                <p class="card-text">{{$product->description}}</p>
+            </div>
+        </div>
+        @endforeach
+    </div>
+    <div class="mt-2 d-flex justify-content-center">
+        {{$products->links()}}
+    </div>
+</div>
+@endsection
